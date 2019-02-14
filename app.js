cefb1847371ca0fd38da692380884dedea7f5dde
rajvishah25 <shah.rajvi54@gmail.com>
2019-02-13 20:28:31 -0800

    axios

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/hello',(req,res)  =>{
+
+
+console.log(req.query);
+res.send('Hello ' + req.query.name);
+});
+app.get('/add',(req ,res)  => res.send('0'));
+
+app.listen(port, () => console.log(`example app listening on port ${port}!`));
+//http://localhost:3000/hello?name=rajvi
